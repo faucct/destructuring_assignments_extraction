d4136920ba54d92efb15bb13b58cbbb5eeb38994
Nikita Sokolov <faucct@gmail.com>
2019-05-19 15:40:09 +0300

    DestructuringAssignmentsExtractorTest::parser

--- src/test/scala/DestructuringAssignmentsExtractorTest.scala
+++ src/test/scala/DestructuringAssignmentsExtractorTest.scala
@@ -1,19 +1,21 @@
 import org.mozilla.javascript.Parser
 
 class DestructuringAssignmentsExtractorTest extends org.scalatest.FunSuite {
-  test("") {
-    import org.mozilla.javascript.CompilerEnvirons
-
-    val environment = new CompilerEnvirons
-
-    environment.setRecordingComments(true)
-
-    val script = new Parser(environment).parse(
+  test("variable declarations") {
+    val script = parser().parse(
       "function ping(arr) {\n  var a = arr[0];\n  var b = arr[1];\n}\n",
       null,
       0
     )
     DestructuringAssignmentsExtractor(script)
     assertResult("function ping(arr) {\n  var [a, b] = arr;\n}\n")(script.toSource(0))
+  }
+
+  private def parser(): Parser = {
+    import org.mozilla.javascript.CompilerEnvirons
+
+    val environment = new CompilerEnvirons
+    environment.setRecordingComments(true)
+    new Parser(environment)
   }
 }
